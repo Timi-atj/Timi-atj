2cf1d9de4f8082e33ba923e2c37dd048934b5fe3
timi.atj <165790857+Timi-atj@users.noreply.github.com>
2025-04-25 09:49:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 import { Platform } from 'react-native';
 
-const LOCAL_IP = "192.168.X.X"; // Replace with your computer's IP
+const LOCAL_IP = "192.168.0.115:8000"; // your IP here
+const USE_DEPLOYED_BACKEND = true;
 
-export const API_BASE_URL =
-  Platform.OS === 'web'
-    ? `http://localhost:8000`
+export const API_BASE_URL = USE_DEPLOYED_BACKEND
+  ? "https://reyne-bot.onrender.com/"
+  : Platform.OS === 'web'
+    ? "http://localhost:8000"
     : `http://${LOCAL_IP}:8000`;
